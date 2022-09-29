b15c4ba7404c40c4dbf1a3754f794bd11427a830
Ngo Viet Duong <nvduong1@cmc.com.vn>
2022-09-29 11:46:26 +0700

    bỏ ghi chú QR Screen

--- src/screens/QRScreen.js
+++ src/screens/QRScreen.js
@@ -1,36 +1,3 @@
-// import React from 'react'
-// import Background from '../components/Background'
-// import Logo from '../components/Logo'
-// import Header from '../components/Header'
-// import Paragraph from '../components/Paragraph'
-// import Button from '../components/Button'
-
-// export default function QRScreen({ navigation }) {
-//   return (
-//     <Background>
-//       <Logo />
-//       <Header>Let’s start QRScreen</Header>
-//       <Paragraph>
-//         Your amazing app starts here. Open you favorite code editor and start
-//         editing this project.
-//       </Paragraph>
-//       <Button
-//         mode="outlined"
-//         onPress={() => {
-//           localStorage.setItem("token", "");
-//           navigation.reset({
-//             index: 0,
-//             routes: [{ name: 'StartScreen' }],
-//           })
-          
-//         }}
-//       >
-//         Logout
-//       </Button>
-//     </Background>
-//   )
-// }
-
 
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';

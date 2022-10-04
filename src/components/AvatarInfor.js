b5b7b3b0f0050954fb798208f7327007b946f9d5
Ngo Viet Duong <nvduong1@cmc.com.vn>
2022-10-04 15:50:17 +0700

    Màn hình cập nhật thông tin với { TextInput } from 'react-native-paper', mode='outlined' trong TextInput

---
+++ src/components/AvatarInfor.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Image, StyleSheet, View, TouchableOpacity, Text } from 'react-native'
+import UserAvatar from 'react-native-user-avatar';
+import { COLORS } from '../Constant/Constant';
+import user from '../Model/user';
+
+export default function AvatarInfor() {
+  return (
+    <View
+      style={{
+        alignItems: 'center',
+      }}
+    >
+      <View
+        style={{
+          width: 150,
+          marginVertical: 20,
+        }}
+      >
+        <UserAvatar
+          size={150}
+          src='https://thumbs.dreamstime.com/b/user-icon-flat-style-isolated-grey-background-user-icon-flat-style-isolated-grey-background-your-design-logo-131213475.jpg'
+          bgColor='null'
+        />
+
+
+        <TouchableOpacity
+          style={{
+            position: 'absolute',
+            right: 0,
+            bottom: 0,
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: COLORS.light_grey,
+              padding: 10,
+              borderRadius: 100,
+            }}
+          >
+            <Image
+              source={require('../assets/camera.png')}
+              style={{
+                width: 30,
+                height: 30,
+              }}
+            />
+          </View>
+
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          alignItems: 'center',
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: 'bold'
+          }}
+        >
+          {user.name}
+        </Text>
+        <Text
+          style={{
+            color: COLORS.light_grey,
+          }}
+        >
+          {user.email}
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+
--- src/screens/addTab/EditInformationScreen.js
+++ src/screens/addTab/EditInformationScreen.js
@@ -1,79 +1,127 @@
 import React from 'react'
 import { COLORS } from '../../Constant/Constant';
-import { Image, View, TouchableOpacity, Text } from 'react-native'
+import { Image, View, TouchableOpacity, Text, StyleSheet } from 'react-native'
 import UserAvatar from 'react-native-user-avatar';
 import user from '../../Model/user';
+import SectionList from 'react-native/Libraries/Lists/SectionList'
+import { TextInput } from 'react-native-paper';
+import AvatarInfor from '../../components/AvatarInfor';
 
+const sectionLists = [
+  {
+    sectionTitle: 'HỌ VÀ TÊN',
+    data: [
+      { label: 'Avatar' }
+    ],
+  },
+  {
+    sectionTitle: 'HỌ VÀ TÊN',
+    data: [
+      { label: "Họ và tên", value: user.name },
+      { label: "Ngày sinh", value: user.birthday },
+      { label: "Giới tính", value: user.gender },
+    ],
+  },
+  {
+    sectionTitle: 'THÔNG TIN LIÊN HỆ',
+    data: [
+      { label: "Số điện thoại", value: user.phoneNumber },
+      { label: "Skype", value: user.skype },
+      { label: "Facebook", value: user.facebook },
+      { label: "Địa chỉ thường trú", value: user.permanent_address },
+      { label: "Địa chỉ tạm trú", value: user.temporary_address },
+    ],
+  },
+  {
+    sectionTitle: 'TRÌNH ĐỘ HỌC VẤN',
+    data: [
+      { label: "Trường", value: user.school },
+      { label: "Trình độ học vấn", value: user.literacy },
+      { label: "Bằng cấp", value: user.degree },
+    ],
+  },
+  {
+    sectionTitle: 'THÔNG TIN CƠ BẢN',
+    data: [
+      { label: "Số CMND", value: user.idNumber },
+      { label: "Ngày cấp CMND", value: user.dateForID },
+      { label: "Nơi cấp CMND", value: user.placeForID },
+    ],
+  },
+  {
+    sectionTitle: 'HỌ VÀ TÊN',
+    data: [
+      { label: 'Cập nhật thông tin' }
+    ],
+  },
+  {
+    sectionTitle: '',
+    data: [
+
+    ],
+  },
+  {
+    sectionTitle: '',
+    data: [
+
+    ],
+  },
+];
 export default function EditInformationScreen({ navigation }) {
+  const [text, setText] = React.useState("");
+
   return (
-    <View
-      style={{
-        alignItems: 'center',
-        padding: 20,
-      }}
-    >
-      <View
-        style={{ 
-          width: 150,
-          marginVertical: 20,
-        }}
-      >
-        <UserAvatar
-          size={150}
-          src='https://thumbs.dreamstime.com/b/user-icon-flat-style-isolated-grey-background-user-icon-flat-style-isolated-grey-background-your-design-logo-131213475.jpg'
-          bgColor='null'
-        />
-
-
-        <TouchableOpacity
-          style={{
-            position: 'absolute',
-            right: 0,
-            bottom: 0,
-          }}
-        >
-          <View
-            style={{
-              backgroundColor: COLORS.light_grey,
-              padding: 10,
-              borderRadius: 100,
-            }}
-          >
-            <Image
-              source={require('../../assets/camera.png')}
-              style={{
-                width: 30,
-                height: 30,
-              }}
-            />
-          </View>
-
-        </TouchableOpacity>
-      </View>
-      <View
-        style={{
-          alignItems: 'center',
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 30,
-            fontWeight: 'bold'
-          }}
-        >
-          {user.name}
-        </Text>
-        <Text
-          style={{
-            color: COLORS.light_grey,
-          }}
-        >
-          {user.email}
-        </Text>
-      </View>
-      
-
-
+    <View>
+      <SectionList
+        style={styles.padding}
+        sections={sectionLists}
+        keyExtractor={(section, index) => section + index}
+        renderSectionHeader={({ section: { sectionTitle } }) => (
+          sectionTitle == 'HỌ VÀ TÊN' ? null :
+            <Text style={styles.header}>{sectionTitle}</Text>
+        )}
+        renderItem={({ item, index }) => (
+          item.label == 'Avatar' ? <AvatarInfor /> :
+            (
+              item.label == 'Cập nhật thông tin' ?
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: COLORS.dark_green,
+                    marginVertical: 5,
+                    paddingVertical: 20,
+                    borderRadius: 5,
+                    alignItems: 'center',
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: 'white',
+                    }}
+                  >CẬP NHẬT THÔNG TIN</Text>
+                </TouchableOpacity> :
+                <TextInput
+                  style={{ marginVertical: 5 }}
+                  mode='outlined'
+                  activeOutlineColor={COLORS.dark_green}
+                  label={item.label}
+                  value={item.value}
+                  onChangeText={text => setText(text)}
+                />
+            )
+        )}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  padding: {
+    paddingHorizontal: 20,
+  },
+  header: {
+    fontSize: 14,
+    marginVertical: 20,
+    color: COLORS.light_grey,
+    fontWeight: 'bold',
+  },
+});
